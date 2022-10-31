38c2ad5e81b5bca4a6285429123d0d4c781ecd55
Santiago Martinez <h3363968@gmail.com>
2022-10-30 22:32:06 -0300

    while

--- scrip.js
+++ scrip.js
@@ -1,4 +1,4 @@
-// Mi idea de proyecto final es poder aplicar javascript al e-commerce que hice en el curso de desarrollo web, lo puse en un html simple 
+// Mi idea de proyecto final es poder aplicar javascript al e-commerce que hice en el curso de desarrollo web, lo puse en un html simple
 
 //simulador para calcular el total de su compra
 function calcularPrecio(
@@ -18,14 +18,18 @@ function calcularPrecio(
 const envio = 800; //valor aproximado del costo de envio promedio dentro de CABA
 
 //le pedimos al usuario el precio de la prenda, la cantidad y el porcentaje del descuento
-let prenda = parseFloat(
-  prompt("Ingrese el precio de la prenda que desea comprar:")
-);
-let cantidad = parseInt(
-  prompt("Ingrese la cantidad de unidades que desea comprar de esta prenda:")
-);
-let descuento = parseInt(prompt("Ingresa tu cupón de descuento:")); //los cupones en este caso serian los porcentajes de descuento
+let ingresar = prompt("Desea calcular el precio de una prenda?");
 
-let precioFinal = calcularPrecio(prenda, cantidad, descuento, envio);
-alert("El precio total de tu compra es $" + precioFinal);
-alert("¡Que lo disfrutes!");
+while (ingresar.toLocaleLowerCase() == si) {
+  let prenda = parseFloat(
+    prompt("Ingrese el precio de la prenda que desea comprar")
+  );
+  let cantidad = parseInt(
+    prompt("Ingrese la cantidad de unidades que desea comprar de esta prenda:")
+  );
+  let descuento = parseInt(prompt("Ingresa tu cupón de descuento:")); //los cupones en este caso serian los porcentajes de descuento
+
+  let precioFinal = calcularPrecio(prenda, cantidad, descuento, envio);
+  alert("El precio total de tu compra es $" + precioFinal);
+  alert("¡Que lo disfrutes!");
+}
